b2d7b17560217b7751c34df42a3a6c26a3af247f
twenty5151 <tsuiyikching@protonmail.com>
2020-05-27 13:19:02 +0100

    Updated show man loop to use AbstractUsers name

--- koneko/koneko/utils.py
+++ koneko/koneko/utils.py
@@ -85,7 +85,7 @@ def show_man_loop():
     print(' ' * 3, '=' * 30)
     print(ui.Image.__doc__)
     print(' ' * 3, '=' * 30)
-    print(ui.Users.__doc__)
+    print(ui.AbstractUsers.__doc__)
     print(' ' * 3, '=' * 30)
     print(ui.IllustFollowGallery.__doc__)
     while True:

c22895bb6d437812cb8bfefac975a0f1477715dd
twenty5151 <tsuiyikching@protonmail.com>
2020-05-29 12:14:00 +0100

    Test all installation commands

--- testing/run.py
+++ testing/run.py
@@ -1,9 +1,22 @@
-"""Tries to run koneko in shell for github workflow. Not to be used with pytest"""
+"""Tries to run koneko in shell for github workflow. Not to be used with pytest.
+For github workflows, the app will crash because it can't read stdin
+You are presumably running this on a terminal that can read stdin, so it will
+hang forever. So this is only for github actions, not for you
+"""
+
 import os
 
-os.system("pip install .")
-os.system("cd ~")
-try:
-    os.system("koneko")
-except EOFError:
-    pass
+def test_launch():
+    curdir = os.getcwd()
+    os.system("cd ~")
+    try:
+        os.system("koneko")
+    except EOFError:
+        pass
+    os.chdir(curdir)
+
+
+for cmd in ("pip install .", "python setup.py install", "python setup.py develop"):
+    os.system(cmd)
+    test_launch()
+    os.system('pip uninstall koneko')

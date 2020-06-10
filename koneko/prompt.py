fe23f248babb1ee3dc5504df47fb20708635d596
twenty5151 <tsuiyikching@protonmail.com>
2020-06-10 22:39:34 +0800

    Moved print before ask_quit to ask_quit

--- koneko/prompt.py
+++ koneko/prompt.py
@@ -11,6 +11,7 @@ TERM = Terminal()
 
 def ask_quit():
     """Ask for quit confirmation, no need to press enter"""
+    print('Are you sure you want to exit?')
     with TERM.cbreak():
         while True:
             ans = TERM.inkey()
@@ -56,6 +57,7 @@ def gallery_like_prompt(gallery):
 
     with TERM.cbreak():
         while True:
+            print("Enter a gallery command:")
             gallery_command = TERM.inkey()
             print(gallery_command, end='', flush=True)
 
@@ -89,10 +91,7 @@ def gallery_like_prompt(gallery):
                 return gallery.handle_prompt(['r'])
 
             elif gallery_command == 'q':
-                print('Are you sure you want to exit?')
                 ask_quit()
-                # If exit cancelled
-                print('Enter a gallery command:')
 
             # Multi char sequence
             if len(keyseqs) == 2 and keyseqs[0].isdigit() and keyseqs[1].isdigit():
@@ -176,7 +175,6 @@ def image_prompt(image):
                 ]))
 
             elif image_prompt_command == 'q':
-                print('Are you sure you want to exit?')
                 ask_quit()
 
             elif image_prompt_command == 'b':
@@ -237,7 +235,6 @@ def user_prompt(user_instance):
                 # Not the end of the sequence yet, continue while block
 
             elif user_prompt_command == 'q':
-                print('Are you sure you want to exit?')
                 ask_quit()
 
             elif user_prompt_command == 'm':

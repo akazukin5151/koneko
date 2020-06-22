cfe7eaffa373edcc417ac72bcf6605a45a2be511
twenty5151 <tsuiyikching@protonmail.com>
2020-06-22 22:09:11 +0800

    Add thumbnail size & gallery spacing assists

--- koneko/lscat_interactive.py
+++ koneko/lscat_interactive.py
@@ -1,6 +1,12 @@
+import os
+import sys
+import time
 from pathlib import Path
 
-from koneko import KONEKODIR, lscat
+from pixcat import Image
+from blessed import Terminal
+
+from koneko import KONEKODIR, lscat, config
 
 
 class FakeData:
@@ -57,7 +63,71 @@ def display_path():
 
 
 def config_assistance():
-    print('todo')
+    term = Terminal()
+
+    size = thumbnail_size(term)
+    gallery_spacing = gallery_page_spacing(term, size)
+
+def thumbnail_size(term):
+    print('=== Thumbnail size ===')
+    print('This will display an image whose thumbnail size can be varied')
+    print('Use +/= to increase the size, and -/_ to decrease it')
+    print('Use q to exit the program, and press enter to confirm the size')
+
+    input('Enter any key to continue\n')
+    os.system('clear')
+    size = 300
+    image = Image(
+        KONEKODIR.parent / 'pics' / '71471144_p0.png'
+    )
+
+    while True:
+        with term.cbreak():
+            image.thumbnail(size).show(align='left', x=0, y=0)
+
+            ans = term.inkey()
+
+            if ans in {'+', '='}:
+                size += 20
+
+            elif ans in {'-', '_'}:
+                image.hide()
+                size -= 20
+
+            elif ans == 'q':
+                sys.exit(0)
+
+            elif ans.code == 343:  # Enter
+                return size
+
+            #elif ans == 't':
+            # TODO: preview a grid with chosen size
+
+
+def gallery_page_spacing(term, thumbnail_size):
+    print('=== Gallery page spacing ===')
+    print('This will display an image, then print newlines.')
+    print('Your desired setting is the number when '
+          'the image completely scrolls out of view')
+
+    input('Enter any key to continue\n')
+    os.system('clear')
+
+    Image(
+        KONEKODIR.parent / 'pics' / '71471144_p0.png'
+    ).thumbnail(thumbnail_size).show(align='left')
+
+    time.sleep(0.5)
+
+    for i in range(term.height + 5):
+        print(i)
+        time.sleep(0.1)
+
+    print('When the image just completely scrolls out of view, '
+          'what is the lowest number?')
+    print('(By default on kitty, ctrl+shift+up/down '
+          'scrolls up/down a line)')
+    return input()
 
 
 if __name__ == '__main__':

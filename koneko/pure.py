7772928d97e3980a3fc282a23186248c131c62f1
twenty5151 <tsuiyikching@protonmail.com>
2020-06-29 18:49:18 +0800

    SIMPLIFIED GENERATE_ORDERS() TO TWO LINES!!!

--- koneko/pure.py
+++ koneko/pure.py
@@ -6,6 +6,7 @@ Most input data come from impure sources (user input or network request), but th
 
 import os
 import re
+from math import floor
 from pathlib import Path
 
 from placeholder import _
@@ -184,15 +185,7 @@ def generate_orders(total_pics: int, artists_count: int) -> 'list[int]':
     0, 30, 31, 32, 1, 33, 34, 35, 2, 36, 37, 38, ...
     a, p,  p,  p,  a, p,  p,  p,  a, ...
     """
-    prev = tuple(range(artists_count, total_pics))
-    order = [None] * total_pics
+    order = [x + artists_count - 1 - floor(x/4) for x in range(total_pics)]
     order[0::4] = range(artists_count)
-
-    j = 0
-    for i in range(len(order)):
-        if order[i] is None:
-            order[i] = prev[j]
-            j += 1
-
     return order
 

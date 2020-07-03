f3e5aa8cadc6f52803fe1e1b0168c9673672d210
twenty5151 <tsuiyikching@protonmail.com>
2020-07-03 12:10:12 +0800

    Add back all cli tests

--- testing/test_cli.py
+++ testing/test_cli.py
@@ -25,43 +25,82 @@ def test_mode1_mode_and_link(monkeypatch, mode):
     assert mock.call_args_list == [call('2232374')]
     assert mock.mock_calls == [call('2232374'), call().start()]
 
-#def test_mode2_link_only(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv',
-#                        (['koneko', 'https://www.pixiv.net/en/artworks/78823485']))
-#    assert cli.process_cli_args() == ('2', '78823485')
-#
-#def test_mode2_mode_and_link(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '2', '78823485']))
-#    assert cli.process_cli_args() == ('2', '78823485')
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'i', '78823485']))
-#    assert cli.process_cli_args() == ('2', '78823485')
-#
-#def test_mode3_mode_and_link(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '3', '78823485']))
-#    assert cli.process_cli_args() == ('3', '78823485')
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'f', '78823485']))
-#    assert cli.process_cli_args() == ('3', '78823485')
-#
-#def test_mode3_no_link(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '3']))
-#    assert cli.process_cli_args() == ('3', '')
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'f']))
-#    assert cli.process_cli_args() == ('3', '')
-#
-#def test_mode4_string_only(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'searchstring']))
-#    assert cli.process_cli_args() == ('4', 'searchstring')
-#
-#def test_mode4_mode_and_string(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '4', 'searchstring']))
-#    assert cli.process_cli_args() == ('4', 'searchstring')
-#
-#def test_mode5_no_link(monkeypatch):
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '5']))
-#    assert cli.process_cli_args() == ('5', '')
-#    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'n']))
-#    assert cli.process_cli_args() == ('5', '')
-#
+def test_mode2_link_only(monkeypatch):
+    monkeypatch.setattr('koneko.cli.sys.argv',
+                        (['koneko', 'https://www.pixiv.net/en/artworks/78823485']))
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.main.ViewPostModeLoop', mock)
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('78823485')]
+    assert mock.mock_calls == [call('78823485'), call().start()]
+
+@pytest.mark.parametrize("mode", ('2', 'i'))
+def test_mode2_mode_and_link(monkeypatch, mode):
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', mode, '78823485']))
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.main.ViewPostModeLoop', mock)
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('78823485')]
+    assert mock.mock_calls == [call('78823485'), call().start()]
+
+@pytest.mark.parametrize("mode", ('3', 'f'))
+def test_mode3_mode_and_link(monkeypatch, mode):
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', mode, '78823485']))
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.main.FollowingUserModeLoop', mock)
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('78823485')]
+    assert mock.mock_calls == [call('78823485'), call().start()]
+
+@pytest.mark.parametrize("mode", ('3', 'f'))
+def test_mode3_no_link(monkeypatch, mode):
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', mode]))
+    monkeypatch.setattr('koneko.cli.main.FollowingUserModeLoop', mock)
+    monkeypatch.setattr('koneko.utils.ask_your_id', lambda x: '78823485')
+
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, '78823485')
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('78823485')]
+    assert mock.mock_calls == [call('78823485'), call().start()]
+
+def test_mode4_string_only(monkeypatch):
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', 'searchstring']))
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.main.SearchUsersModeLoop', mock)
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('searchstring')]
+    assert mock.mock_calls == [call('searchstring'), call().start()]
+
+def test_mode4_mode_and_string(monkeypatch):
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', '4', 'searchstring']))
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.main.SearchUsersModeLoop', mock)
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_count == 1
+    assert mock.call_args_list == [call('searchstring')]
+    assert mock.mock_calls == [call('searchstring'), call().start()]
+
+@pytest.mark.parametrize("mode", ('5', 'n'))
+def test_mode5_no_link(monkeypatch, mode):
+    mock = Mock()
+    monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', mode]))
+    monkeypatch.setattr('koneko.cli.main.illust_follow_mode_loop', mock)
+
+    args = cli.handle_vh()
+    assert cli.launch_mode(args, True)
+    assert mock.call_args_list == [call()]
+
 @pytest.mark.parametrize("arg", ('-v', '--version'))
 def test_version(monkeypatch, arg):
     monkeypatch.setattr('koneko.cli.sys.argv', (['koneko', arg]))

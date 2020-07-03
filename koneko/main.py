4ddfc0548d613670697bec3384b40b07a54ddf23
twenty5151 <tsuiyikching@protonmail.com>
2020-07-03 14:53:14 +0800

    Loop class docstrings

--- koneko/main.py
+++ koneko/main.py
@@ -94,6 +94,21 @@ class AbstractLoop(ABC):
         self._raw_answer: str
         self.mode: 'ui'
 
+    @abstractmethod
+    def _prompt_url_id(self) -> str:
+        """Define self._raw_answer here"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _go_to_mode(self):
+        """Define self.mode here"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __str__(self):
+        """Return a hard-coded string of the current mode number"""
+        raise NotImplementedError
+
     def start(self):
         """Ask for further info if not provided, then proceed to mode"""
         while True:
@@ -114,11 +129,6 @@ class AbstractLoop(ABC):
             else:
                 self._go_to_mode()
 
-    @abstractmethod
-    def _prompt_url_id(self) -> str:
-        """Define self._raw_answer here"""
-        raise NotImplementedError
-
     def _process_raw_answer(self) -> str:
         """Process self._raw_answer here into self._user_input"""
         self._user_input = pure.process_user_url(self._raw_answer)
@@ -138,15 +148,6 @@ class AbstractLoop(ABC):
             logger = utils.setup_history_log()
             logger.info(str(self) + ': ' + self._user_input)
 
-    @abstractmethod
-    def _go_to_mode(self):
-        """Define self.mode here"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def __str__(self):
-        """Return a hard-coded string of the current mode number"""
-        raise NotImplementedError
 
 
 class ArtistModeLoop(AbstractLoop):
@@ -155,9 +156,11 @@ class ArtistModeLoop(AbstractLoop):
     before proceeding
     """
     def _prompt_url_id(self) -> str:
+        """Implements abstractmethod: prompt for artist ID or url"""
         self._raw_answer = input('Enter artist ID or url:\n')
 
     def _go_to_mode(self):
+        """Implements abstractmethod: go to mode 1"""
         os.system('clear')
         self.mode = ui.ArtistGallery(self._user_input)
         prompt.gallery_like_prompt(self.mode)
@@ -165,6 +168,7 @@ class ArtistModeLoop(AbstractLoop):
         main()
 
     def __str__(self):
+        """Implements abstractmethod: return string of mode number"""
         return '1'
 
 
@@ -174,19 +178,22 @@ class ViewPostModeLoop(AbstractLoop):
     before proceeding
     """
     def _prompt_url_id(self) -> str:
+        """Implements abstractmethod: prompt for post url or ID"""
         self._raw_answer = input('Enter pixiv post url or ID:\n')
 
     def _process_raw_answer(self) -> str:
-        """Overriding base class to account for 'illust_id' cases"""
+        """Overrides base method for different processor function"""
         self._user_input = pure.process_artwork_url(self._raw_answer)
 
     def _go_to_mode(self):
+        """Implements abstractmethod: Go to mode 2"""
         os.system('clear')
         ui.view_post_mode(self._user_input)
         # After backing
         main()
 
     def __str__(self):
+        """Implements abstractmethod: return string of mode number"""
         return '2'
 
 
@@ -198,20 +205,26 @@ class FollowingUserModeLoop(AbstractLoop):
     skipped
     """
     def _prompt_url_id(self) -> str:
+        """Implements abstractmethod: prompt for artist ID or url"""
         self._raw_answer = input('Enter your pixiv ID or url: ')
 
     def _go_to_mode(self):
+        """Implements abstractmethod: Go to mode 3"""
         os.system('clear')
         self.mode = ui.FollowingUsers(self._user_input)
         prompt.user_prompt(self.mode)
         main()
 
     def start(self, your_id=None):
+        """Complements base method: If ID not given, ask if config ID should be used,
+        or enter a custom ID
+        """
         if your_id:
             self._user_input = utils.ask_your_id(your_id)
         super().start()
 
     def __str__(self):
+        """Implements abstractmethod: return string of mode number"""
         return '3'
 
 class SearchUsersModeLoop(AbstractLoop):
@@ -220,28 +233,31 @@ class SearchUsersModeLoop(AbstractLoop):
     before proceeding
     """
     def _prompt_url_id(self) -> str:
+        """Implements abstractmethod: prompt for search string"""
         self._raw_answer = input('Enter search string:\n')
 
     def _process_raw_answer(self) -> str:
-        """the 'url or id' name doesn't really apply; accepts all strings"""
+        """Overrides base method: nothing to process"""
         self._user_input = self._raw_answer
 
     def _validate_input(self) -> bool:
-        """Overriding base class: all inputs are valid"""
+        """Overrides base method: all inputs are valid"""
         return True
 
     def _go_to_mode(self):
+        """Implements abstractmethod: Go to mode 4"""
         os.system('clear')
         self.mode = ui.SearchUsers(self._user_input)
         prompt.user_prompt(self.mode)
         main()
 
     def __str__(self):
+        """Implements abstractmethod: return string of mode number"""
         return '4'
 
 
 def illust_follow_mode_loop():
-    """Immediately goes to IllustFollow()"""
+    """Immediately goes to ui.IllustFollowGallery()"""
     mode = ui.IllustFollowGallery()
     prompt.gallery_like_prompt(mode)
     # After backing

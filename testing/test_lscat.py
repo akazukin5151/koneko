670bb44a7348320eb544c4a0571b960237e3d9db
twenty5151 <tsuiyikching@protonmail.com>
2020-07-03 22:23:20 +0800

    test_lscat now refers to global TERM

--- testing/test_lscat.py
+++ testing/test_lscat.py
@@ -138,8 +138,8 @@ def test_TrackDownloadsUser2(monkeypatch):
 def test_generate_page(monkeypatch):
     mocked_pixcat = Mock()
     monkeypatch.setattr("koneko.lscat.Image", lambda *a, **k: mocked_pixcat)
-    monkeypatch.setattr("koneko.config.Terminal.width", 100)
-    monkeypatch.setattr("koneko.config.Terminal.height", 20)
+    monkeypatch.setattr("koneko.Terminal.width", 100)
+    monkeypatch.setattr("koneko.Terminal.height", 20)
 
     test_pics = [f"{str(idx).rjust(3, '0')}_test"
                  for idx in list(range(30))]
@@ -171,8 +171,8 @@ def test_generate_page(monkeypatch):
 def test_generate_users(monkeypatch):
     mocked_pixcat = Mock()
     monkeypatch.setattr("koneko.lscat.Image", lambda *a, **k: mocked_pixcat)
-    monkeypatch.setattr("koneko.config.Terminal.width", 100)
-    monkeypatch.setattr("koneko.config.Terminal.height", 20)
+    monkeypatch.setattr("koneko.Terminal.width", 100)
+    monkeypatch.setattr("koneko.Terminal.height", 20)
 
     test_pics = [f"{str(idx).rjust(3, '0')}_test"
                  for idx in list(range(120))]

69553f30074134aec6da7fe5a8f878658bbcea6c
twenty5151 <tsuiyikching@protonmail.com>
2020-07-05 20:08:06 +0800

    Use pytest tmp_path in test_files

--- testing/test_files.py
+++ testing/test_files.py
@@ -19,35 +19,31 @@ def test_verify_full_download():
     # The above code will remove the file
     os.system("touch testing/files/not_an_image.txt")
 
-def test_dir_not_empty():
+def test_dir_not_empty(tmp_path):
     class FakeData:
         def __init__(self):
-            self.download_path = Path('testing/files/empty_dir')
+            self.download_path = tmp_path
             self.first_img = "004_祝！！！.jpg"
             self.all_names = ["004_祝！！！.jpg", '008_77803142_p0.png', '017_ミコニャン.jpg']
 
     data = FakeData()
 
-    try:
-        # Test dir exists but is empty
-        Path('testing/files/empty_dir').mkdir()
-        data.download_path = Path('testing/files/empty_dir')
-        assert files.dir_not_empty(data) is False
+    # Test dir exists but is empty
+    tmp_path.mkdir(exist_ok=True)
+    data.download_path = tmp_path
+    assert files.dir_not_empty(data) is False
 
-        # Copy .koneko and only one image to that dir
-        os.system('touch testing/files/empty_dir/.koneko')
-        os.system('cp testing/files/004_祝！！！.jpg testing/files/empty_dir/')
+    # Copy .koneko and only one image to that dir
+    (tmp_path / '.koneko').touch()
+    os.system(f'cp testing/files/004_祝！！！.jpg {tmp_path}')
 
-        assert files.dir_not_empty(data) is False
+    assert files.dir_not_empty(data) is False
 
-        # Copy all images to dir
-        for f in ('008_77803142_p0.png', '017_ミコニャン.jpg'):
-            os.system(f'cp testing/files/{f} testing/files/empty_dir/')
+    # Copy all images to dir
+    for f in ('008_77803142_p0.png', '017_ミコニャン.jpg'):
+        os.system(f'cp testing/files/{f} {tmp_path}')
 
-        assert files.dir_not_empty(data)
-
-    finally:
-        os.system('rm -r testing/files/empty_dir')
+    assert files.dir_not_empty(data)
 
     # Test Throw some errors
     class FakeData:

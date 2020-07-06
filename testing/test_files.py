ad09ddaa2116bcd0278aacda998524972a328a2f
twenty5151 <tsuiyikching@protonmail.com>
2020-07-06 12:33:42 +0800

    Add more tests for files

--- testing/test_files.py
+++ testing/test_files.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from collections import namedtuple
 
 import pytest
 
@@ -18,6 +19,23 @@ def test_verify_full_download():
     assert files.verify_full_download("testing/files/not_an_image.txt") is False
     # The above code will remove the file
     os.system("touch testing/files/not_an_image.txt")
+
+
+@pytest.mark.parametrize('mkdir', (True, False))
+def test_remove_dir_if_exist(tmp_path, mkdir):
+    path = tmp_path / 'test'
+    if mkdir:
+        path.mkdir()
+    FakeData = namedtuple('data', ('download_path',))
+    data = FakeData(path)
+    files.remove_dir_if_exist(data)
+    assert not path.is_dir()
+
+
+def test_filter_history(tmp_path):
+    (tmp_path / 'history').touch()
+    assert files.filter_history(tmp_path) == []
+
 
 def test_dir_not_empty(tmp_path):
     class FakeData:
@@ -67,3 +85,49 @@ def test_dir_not_empty(tmp_path):
 
     data = FakeData()
     assert files.dir_not_empty(data)
+
+
+def test_filter_dir_1(monkeypatch, tmp_path):
+    monkeypatch.setattr('koneko.files.KONEKODIR', tmp_path)
+    (tmp_path / '1234').touch()
+    (tmp_path / 'testgallery').touch()
+    (tmp_path / 'notanumber').touch()
+
+    assert files.filter_dir(['1']) == ['testgallery', '1234']
+
+
+def test_filter_dir_2(monkeypatch, tmp_path):
+    monkeypatch.setattr('koneko.files.KONEKODIR', tmp_path)
+    (tmp_path / '1234').mkdir()
+    (tmp_path / '1234' / 'individual').touch()
+    (tmp_path / '5678').mkdir()
+    (tmp_path / 'testgallery').touch()
+    (tmp_path / 'notanumber').touch()
+
+    assert files.filter_dir(['2']) == ['1234']
+
+
+def test_filter_dir_3(monkeypatch, tmp_path):
+    monkeypatch.setattr('koneko.files.KONEKODIR', tmp_path)
+    (tmp_path / '1234').touch()
+    (tmp_path / 'testuser').touch()
+    (tmp_path / 'following').touch()
+
+    assert files.filter_dir(['3']) == ['following', 'testuser']
+
+
+def test_filter_dir_4(monkeypatch, tmp_path):
+    monkeypatch.setattr('koneko.files.KONEKODIR', tmp_path)
+    (tmp_path / '1234').touch()
+    (tmp_path / 'search').touch()
+    (tmp_path / 'notanumber').touch()
+
+    assert files.filter_dir(['4']) == ['search']
+
+def test_filter_dir_5(monkeypatch, tmp_path):
+    monkeypatch.setattr('koneko.files.KONEKODIR', tmp_path)
+    (tmp_path / '1234').touch()
+    (tmp_path / 'illustfollow').touch()
+    (tmp_path / 'notanumber').touch()
+
+    assert files.filter_dir(['5']) == ['illustfollow']

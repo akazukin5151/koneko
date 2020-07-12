36b48c0b600dac837313c5aeda7016d2beb41f1c
twenty5151 <tsuiyikching@protonmail.com>
2020-07-12 14:04:55 +0800

    Fixed mode 2 + mode 3/4/5 filter only showing 'individual'

--- koneko/picker.py
+++ koneko/picker.py
@@ -186,7 +186,10 @@ def handle_cd(path, selected_dir: 'path', modes: 'list[str]') -> ('path', 'list[
 def actions_from_dir(path: 'path', modes: 'list[str]') -> 'list[str]':
     if path == KONEKODIR and modes:  # Filter active
         return try_filter_dir(modes)
-    if '2' in modes and '1' not in modes and 'individual' not in str(path):
+    if ('2' in modes
+            and '1' not in modes
+            and 'individual' not in str(path)
+            and str(path).isdigit()):
         return ['individual']
     return files.filter_history(path)
 

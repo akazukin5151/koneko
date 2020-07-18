5d67114eb2761e73b2c13b99a6de4850f97e2487
twenty5151 <tsuiyikching@protonmail.com>
2020-07-18 15:02:40 +0800

    Fixed "a image" grammar

--- koneko/prompt.py
+++ koneko/prompt.py
@@ -27,7 +27,7 @@ def ask_quit():
 
 def ask_wait_user_input(keyseqs: 'list[str]', view_name: str) -> str:
     if not keyseqs:
-        print(f'Enter a {view_name} view command:')
+        print(f'Enter {view_name} view command:')
     command = TERM.inkey()
     print(command, end='', flush=True)
     return command
@@ -117,7 +117,7 @@ def gallery_like_prompt(gallery):
                 keyseqs = []
 
             # 2. Wait for user input
-            gallery_command = ask_wait_user_input(keyseqs, 'gallery')
+            gallery_command = ask_wait_user_input(keyseqs, 'a gallery')
 
             # 3. Single char input with action that leaves prompt
             if gallery_command == 'b':
@@ -154,7 +154,7 @@ def image_prompt(image):
                 keyseqs = []
 
             # 2. Ask and wait for user input
-            image_prompt_command = ask_wait_user_input(keyseqs, 'image')
+            image_prompt_command = ask_wait_user_input(keyseqs, 'an image')
 
             # 3. Single char input with action that leaves prompt
             if image_prompt_command == 'b':
@@ -190,7 +190,7 @@ def user_prompt(user):
                 return user.go_artist_mode(pure.concat_seqs_to_int(keyseqs))
 
             # 2. Ask and wait for user input
-            user_prompt_command = ask_wait_user_input(keyseqs, 'user')
+            user_prompt_command = ask_wait_user_input(keyseqs, 'a user')
 
             # 3. Single char input with action that leaves prompt
             if user_prompt_command == 'r':
